251d579abecdd5d0e363304ba993ca10d473f870
Cherkesky <guy@cherkesky.com>
2020-05-07 16:29:58 -0500

    aggragation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace inheritance
 {
+
+
+
+
   class Program
   {
     /*
@@ -35,7 +41,7 @@ balls in our program.
     }
 
 
-    
+
 
  // COMPOSITION
 class Pancreas
@@ -72,16 +78,55 @@ class Body
 
 
 
+    //AGGREGATION
+class Customer
+{
+    public string AccountNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
 
+class Bank
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
 
+    // Customers are aggregated into the bank
+    public List<Customer> Customers { get; set; } = new List<Customer>();
 
+}
 
 
 
 
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-    }
+        Bank FirstTennessee = new Bank() {
+            Name = "First Tennessee",
+            Address = "100 Infinity Way"
+        };
+
+        // Create a customer
+        Customer steve = new Customer();
+        steve.FirstName = "Steve";
+        steve.LastName = "Brownlee";
+        steve.AccountNumber = "110405821101";
+
+        // Add Steve to First Tennessee's list of customers
+        FirstTennessee.Customers.Add(steve);
+
+        // Add another customer using in-line, concise syntax
+        FirstTennessee.Customers.Add(
+            new Customer() {
+            FirstName = "Caitlin",
+            LastName = "Stein",
+            AccountNumber = "0592382394589" }
+        );
+
+        // Iterate over the list of customers and display the first name
+        foreach (Customer cust in FirstTennessee.Customers)
+        {
+            Console.WriteLine(cust.FirstName);
+        }    }
   }
 }
